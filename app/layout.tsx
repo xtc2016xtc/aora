c522e20640d242a38ec0a1db04dec35ec4f03d35
xtc2016xtc <728611827@qq.com>
2024-09-02 17:52:18 +0800

    更新,初始化

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 
 import { cn } from "@/lib/utils";
+import React from "react";
 
 const fontSans = FontSans({
     subsets: ["latin"],
