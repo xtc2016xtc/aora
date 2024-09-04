3fd2cefe8ab09b001b95ce049a52e4c464215ed4
xtc2016xtc <728611827@qq.com>
2024-09-04 15:33:56 +0800

    完善

--- components/compentsCustom/CustomFormField.tsx
+++ components/compentsCustom/CustomFormField.tsx
@@ -1,8 +1,34 @@
-const CustomFormField = () => {
-    return (
-        <div>
+"use client"
+import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Control} from "react-hook-form";
+import {FormFieldType} from "@/components/forms/PatientForm";
 
-        </div>
+interface CustomProps {
+    control: Control<any>,
+    name: string,
+    platform:string,
+    fieldType:FormFieldType
+}
+
+const CustomFormField = ({control,name,platform,fieldType}:CustomProps) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>{name}</FormLabel>
+                    <FormControl>
+                        <Input placeholder={platform} {...field} />
+                    </FormControl>
+                    <FormDescription>
+                        This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
     )
 }
 export default CustomFormField
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -2,22 +2,47 @@
 import {Form} from "@/components/ui/form";
 import CustomFormField from "@/components/compentsCustom/CustomFormField";
 import SubmitButton from "@/components/button/SubmitButton";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 import React, {useState} from "react";
+import { z } from "zod"
+const formSchema = z.object({
+    username: z.string().min(2,{
+        message:"最大输入俩个字符"
+    })
+})
+
+export enum FormFieldType {
+    INPUT="input",
+}
 
 const PatientForm = () => {
     const [isLoading, setIsLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
     return (
-        /*@ts-ignore*/
-        <Form>
-            <form className="flex-1 space-y-6">
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
                 <section className="mb-12 space-y-4">
                     <h1 className="header">你好👋</h1>
                     <p className="text-dark-700">可以开始预约医生了.</p>
                 </section>
 
-                <CustomFormField/>
-                <CustomFormField/>
-                <CustomFormField/>
+                <CustomFormField
+                    control={form.control}
+                    fieldType={FormFieldType.INPUT}
+                    name="username"
+                    platform="账户"
+                />
 
                 <SubmitButton isLoading={isLoading}>开始</SubmitButton>
             </form>
