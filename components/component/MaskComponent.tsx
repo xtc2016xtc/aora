bb1ce36d3554b2001f0e65c99b8d8c5df40266e7
xtc2016xtc <728611827@qq.com>
2024-09-19 15:45:57 +0800

    增加隐私蒙版内容

--- components/component/MaskComponent.tsx
+++ components/component/MaskComponent.tsx
@@ -17,19 +17,16 @@ const MaskComponent: React.FC<MaskComponentProps> = ({ maskType }) => {
             {maskType.treatmentConsent && (
                 <div className="bg-white p-6 rounded-lg shadow-lg text-center">
                     <p>这里是关于治疗同意的详细信息。</p>
-                    {/* 更多内容... */}
                 </div>
             )}
             {maskType.disclosureConsent && (
                 <div className="bg-white p-6 rounded-lg shadow-lg text-center">
                     <p>这里是关于信息使用的详细信息。</p>
-                    {/* 更多内容... */}
                 </div>
             )}
             {maskType.privacyConsent && (
                 <div className="bg-white p-6 rounded-lg shadow-lg text-center">
                     <p>这里是关于隐私条款的详细信息。</p>
-                    {/* 更多内容... */}
                 </div>
             )}
         </div>
